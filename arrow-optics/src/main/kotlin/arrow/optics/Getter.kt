329ad269a4cfdc863af3cf68122ccaa2166af865
franciscodr <francisco.d@47deg.com>
2020-12-27 19:47:38 +0100

    Convert interface to fun interface

--- arrow-optics/src/main/kotlin/arrow/optics/Getter.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/Getter.kt
@@ -21,7 +21,7 @@ import arrow.typeclasses.Monoid
  * @param A the focus of a [Getter]
  */
 @higherkind
-interface Getter<S, A> : GetterOf<S, A> {
+fun interface Getter<S, A> : GetterOf<S, A> {
 
   /**
    * Get the focus of a [Getter]
@@ -36,13 +36,6 @@ interface Getter<S, A> : GetterOf<S, A> {
      * [Getter] that takes either [S] or [S] and strips the choice of [S].
      */
     fun <S> codiagonal(): Getter<Either<S, S>, S> = Getter { aa -> aa.fold(::identity, ::identity) }
-
-    /**
-     * Invoke operator overload to create a [Getter] of type `S` with focus `A`.
-     */
-    operator fun <S, A> invoke(get: (S) -> A) = object : Getter<S, A> {
-      override fun get(s: S): A = get(s)
-    }
   }
 
   /**
