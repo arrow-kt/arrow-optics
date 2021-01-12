2fe73edbaa89f592d5ef0a764e850f933cac9f27
Tylos <grtylos@gmail.com>
2021-01-12 12:38:11 +0100

    Move generated code and remove @extension

--- arrow-optics/src/main/kotlin/arrow/optics/extensions/SetKAt.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/extensions/SetKAt.kt
@@ -2,7 +2,6 @@ package arrow.optics.extensions
 
 import arrow.core.SetK
 import arrow.core.k
-import arrow.extension
 import arrow.optics.Lens
 import arrow.optics.PLens
 import arrow.optics.typeclasses.At
@@ -10,7 +9,6 @@ import arrow.optics.typeclasses.At
 /**
  * [At] instance definition for [SetK].
  */
-@extension
 interface SetKAt<A> : At<SetK<A>, A, Boolean> {
   override fun at(i: A): Lens<SetK<A>, Boolean> = PLens(
     get = { it.contains(i) },
