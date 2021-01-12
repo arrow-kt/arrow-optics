a55b5d60fc7b0609514ff146ff03d0d256a0d22f
franciscodr <francisco.d@47deg.com>
2021-01-12 16:35:04 +0100

    Deprecate extension methods for NonEmptyList

--- arrow-optics/src/main/kotlin/arrow/optics/std/nonemptylist.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/std/nonemptylist.kt
@@ -1,6 +1,13 @@
 package arrow.optics
 
+import arrow.Kind
 import arrow.core.NonEmptyList
+import arrow.core.left
+import arrow.core.right
+import arrow.core.toT
+import arrow.optics.typeclasses.FilterIndex
+import arrow.optics.typeclasses.Index
+import arrow.typeclasses.Applicative
 
 /**
  * [Lens] to operate on the head of a [NonEmptyList]
@@ -17,3 +24,41 @@ fun <A> NonEmptyList.Companion.tail(): Lens<NonEmptyList<A>, List<A>> = Lens(
   get = NonEmptyList<A>::tail,
   set = { nel, newTail -> NonEmptyList(nel.head, newTail) }
 )
+
+/**
+ * [Traversal] for [NonEmptyList] that has focus in each [A].
+ *
+ * @receiver [PTraversal.Companion] to make it statically available.
+ * @return [Traversal] with source [NonEmptyList] and focus every [A] of the source.
+ */
+fun <A> PTraversal.Companion.nonEmptyList(): Traversal<NonEmptyList<A>, A> =
+  object : Traversal<NonEmptyList<A>, A> {
+    override fun <F> modifyF(FA: Applicative<F>, s: NonEmptyList<A>, f: (A) -> Kind<F, A>): Kind<F, NonEmptyList<A>> =
+      s.traverse(FA, f)
+  }
+
+/**
+ * [FilterIndex] instance definition for [NonEmptyList].
+ */
+fun <A> FilterIndex.Companion.nonEmptyList(): FilterIndex<NonEmptyList<A>, Int, A> = FilterIndex { p ->
+  object : Traversal<NonEmptyList<A>, A> {
+    override fun <F> modifyF(FA: Applicative<F>, s: NonEmptyList<A>, f: (A) -> Kind<F, A>): Kind<F, NonEmptyList<A>> =
+      s.all.mapIndexed { index, a -> a toT index }
+        .let(NonEmptyList.Companion::fromListUnsafe)
+        .traverse(FA) { (a, j) -> if (p(j)) f(a) else FA.just(a) }
+  }
+}
+
+/**
+ * [Index] instance definition for [NonEmptyList].
+ */
+fun <A> Index.Companion.nonEmptyList(): Index<NonEmptyList<A>, Int, A> = Index { i ->
+  POptional(
+    getOrModify = { l -> l.all.getOrNull(i)?.right() ?: l.left() },
+    set = { l, a ->
+      NonEmptyList.fromListUnsafe(
+        l.all.mapIndexed { index: Int, aa: A -> if (index == i) a else aa }
+      )
+    }
+  )
+}
