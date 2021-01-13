265567d444782b16958bdc573c89866c76ba36a5
franciscodr <francisco.d@47deg.com>
2021-01-13 18:22:25 +0100

    Add deprecation messages for MapK

--- arrow-optics/src/main/kotlin/arrow/optics/extensions/mapk.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/extensions/mapk.kt
@@ -22,6 +22,13 @@ import arrow.typeclasses.Applicative
 /**
  * [At] instance definition for [MapK].
  */
+@Deprecated(
+  "Typeclass interface implementation will not be exposed directly anymore",
+  ReplaceWith(
+    "At.map<K, V>()",
+    "arrow.optics.map", "arrow.optics.typeclasses.At"),
+  DeprecationLevel.WARNING
+)
 interface MapKAt<K, V> : At<MapK<K, V>, K, Option<V>> {
   override fun at(i: K): Lens<MapK<K, V>, Option<V>> = PLens(
     get = { it.fix().getOption(i) },
@@ -41,6 +48,13 @@ interface MapKAt<K, V> : At<MapK<K, V>, K, Option<V>> {
  * @receiver [MapK.Companion] to make it statically available.
  * @return [Traversal] with source [MapK] and focus every [V] of the source.
  */
+@Deprecated(
+  "MapK is being deprecated. Use the instance for Map from the companion object of the typeclass.",
+  ReplaceWith(
+    "Traversal.map<K, V>()",
+    "arrow.optics.Traversal", "arrow.optics.map"),
+  DeprecationLevel.WARNING
+)
 fun <K, V> MapK.Companion.traversal(): Traversal<MapK<K, V>, V> = object : Traversal<MapK<K, V>, V> {
   override fun <F> modifyF(FA: Applicative<F>, s: MapK<K, V>, f: (V) -> Kind<F, V>): Kind<F, MapK<K, V>> =
     s.traverse(FA, f)
@@ -49,6 +63,13 @@ fun <K, V> MapK.Companion.traversal(): Traversal<MapK<K, V>, V> = object : Trave
 /**
  * [Each] instance definition for [Map].
  */
+@Deprecated(
+  "Each is being deprecated. Use the instance for Map from Traversal's companion object instead.",
+  ReplaceWith(
+    "Traversal.map<K, V>()",
+    "arrow.optics.Traversal", "arrow.optics.map"),
+  DeprecationLevel.WARNING
+)
 interface MapKEach<K, V> : Each<MapK<K, V>, V> {
   override fun each(): Traversal<MapK<K, V>, V> =
     MapK.traversal()
@@ -57,6 +78,13 @@ interface MapKEach<K, V> : Each<MapK<K, V>, V> {
 /**
  * [FilterIndex] instance definition for [Map].
  */
+@Deprecated(
+  "Typeclass interface implementation will not be exposed directly anymore",
+  ReplaceWith(
+    "FilterIndex.map<K, V>()",
+    "arrow.optics.map", "arrow.optics.typeclasses.FilterIndex"),
+  DeprecationLevel.WARNING
+)
 interface MapKFilterIndex<K, V> : FilterIndex<MapK<K, V>, K, V> {
   override fun filter(p: (K) -> Boolean): Traversal<MapK<K, V>, V> = object : Traversal<MapK<K, V>, V> {
     override fun <F> modifyF(FA: Applicative<F>, s: MapK<K, V>, f: (V) -> Kind<F, V>): Kind<F, MapK<K, V>> = FA.run {
@@ -72,6 +100,13 @@ interface MapKFilterIndex<K, V> : FilterIndex<MapK<K, V>, K, V> {
 /**
  * [Index] instance definition for [Map].
  */
+@Deprecated(
+  "Typeclass interface implementation will not be exposed directly anymore",
+  ReplaceWith(
+    "Index.map<K, V>()",
+    "arrow.optics.map", "arrow.optics.typeclasses.Index"),
+  DeprecationLevel.WARNING
+)
 interface MapKIndex<K, V> : Index<MapK<K, V>, K, V> {
   override fun index(i: K): Optional<MapK<K, V>, V> = POptional(
     getOrModify = { it[i]?.right() ?: it.left() },
