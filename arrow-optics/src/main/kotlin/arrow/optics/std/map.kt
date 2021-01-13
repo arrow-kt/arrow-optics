283145ed4b2b91a9f4e006ddacec2a710974487d
franciscodr <francisco.d@47deg.com>
2021-01-13 14:16:21 +0100

    Deprecate Iso instance from MapK to SetK

--- arrow-optics/src/main/kotlin/arrow/optics/std/map.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/std/map.kt
@@ -16,9 +16,24 @@ import arrow.typeclasses.Applicative
 /**
  * [Iso] that defines the equality between a Unit value [Map] and a [Set] with its keys
  */
+@Deprecated(
+  "MapK is being deprecated, use the function defined for Map instead.",
+  ReplaceWith(
+    "Iso.mapToSet<K>()",
+    "arrow.optics.Iso", "arrow.optics.mapToSet"
+  ),
+  DeprecationLevel.WARNING)
 fun <K> MapK.Companion.toSetK(): Iso<MapK<K, Unit>, SetK<K>> = Iso(
   get = { it.keys.k() },
   reverseGet = { keys -> keys.map { it to Unit }.toMap().k() }
+)
+
+/**
+ * [Iso] that defines the equality between a Unit value [Map] and a [Set] with its keys
+ */
+fun <K> PIso.Companion.mapToSet(): Iso<Map<K, Unit>, Set<K>> = Iso(
+  get = { it.keys },
+  reverseGet = { keys -> keys.map { it to Unit }.toMap() }
 )
 
 fun <K, V> At.Companion.map(): At<Map<K, V>, K, Option<V>> = At { i ->
